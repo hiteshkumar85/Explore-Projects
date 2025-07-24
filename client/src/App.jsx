9a6272957982b2826212bd5892f5e20c8a07fef0
Gaurav Tomar <gauravtomar@977gmail.com>
2025-07-24 22:41:48 +0530

    Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,25 @@
-import './App.css'
+import React from "react";
+
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+import Navbar from "./Navbar";
+import Home from "./pages/Home"
+import Portfolio from "./pages/Portfolio"
+import Solution from "./pages/Solution"
+import Dashboard from "./pages/Dashboard"
 
 function App() {
 
   return (
-    <div>
-      <h1>hitesh bhai or kitne hint doge....</h1>
-    </div>
-  )
+      <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/solution" element={<Solution />} />
+        <Route path ="/dashboard" element={<Dashboard/>}/>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App
---
+++ client/src/pages/Solution.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Solution = () => <h1>Welcome to the Solution Page</h1>;
+
+export default Solution;
