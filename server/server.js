5553e02fa9326b4bcf761b2f0b96997aac341cc7
Gaurav Tomar <gauravtomar977@gmail.com>
2025-07-24 15:17:05 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const port = 3000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Vijay bhai ye backend hai.');
+  res.send('ye gajni bhai ka backend hai');
 });
 
 app.listen(port, () => {
